71f0f6c1a99e4a7c3b9cf64d2cc0329fb13e2482
Bill Hellier <bill.hellier@gmail.com>
2019-12-29 20:52:13 -0500

    Added autoreply property to hreads and a toggle function for it. Added an auto command that invokes it.

---
+++ src/modules/auto.js
@@ -0,0 +1,10 @@
+module.exports = ({ bot, knex, config, commands }) => {
+  commands.addInboxThreadCommand('auto', [], async (msg, args, thread) => {
+    const newState = await thread.toggleAutoreply();
+    if (newState) {
+      await thread.postSystemMessage(`Thread auto-reply mode ENABLED. All messages you send here will be relayed directly, except commands.`);
+    } else {
+      await thread.postSystemMessage(`Thread auto-reply mode DISABLED. Only explicit reply commands will send messages to the user.`);
+    }
+  });
+};
