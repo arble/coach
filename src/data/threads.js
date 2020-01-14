8342aa65bcf2f271c145d2669c8297c3ba482c18
Bill Hellier <bill.hellier@gmail.com>
2020-01-13 23:25:12 -0500

    null check insufficient. Make it an empty object when initialising the thread

--- src/data/threads.js
+++ src/data/threads.js
@@ -157,7 +157,8 @@ async function createNewThreadForUser(user, quiet = false, ignoreRequirements = 
     user_name: `${user.username}#${user.discriminator}`,
     channel_id: createdChannel.id,
     created_at: moment.utc().format('YYYY-MM-DD HH:mm:ss'),
-    gather_state: quiet ? THREAD_GATHER_INFO.COMPLETE : THREAD_GATHER_INFO.PLATFORM
+    gather_state: quiet ? THREAD_GATHER_INFO.COMPLETE : THREAD_GATHER_INFO.PLATFORM,
+    autoreply_users: {}
   });
 
   const newThread = await findById(newThreadId);
