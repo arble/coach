d5e5db4fb6b6f8b7ce2dd6247a7dc89f317abe16
Bill Hellier <bill.hellier@gmail.com>
2020-01-14 18:58:25 -0500

    Added a !claim command, as a shorthand for moving from waiting threads. Will place the channel in a category named for the claiming user, creating it if necessary.

---
+++ src/modules/claim.js
@@ -0,0 +1,35 @@
+const config = require('../config');
+const Eris = require('eris');
+const transliterate = require("transliteration");
+const erisEndpoints = require('eris/lib/rest/Endpoints');
+
+module.exports = ({ bot, knex, config, commands }) => {
+  commands.addInboxThreadCommand('claim', [], async (msg, args, thread) => {
+    const username = transliterate.slugify(`${msg.author.username}`);
+    const catStr = `${username}#${msg.author.discriminator}`;
+
+    // only allow claiming of threads that are still in "Waiting Threads"
+    if (msg.channel.parentID !== config.categoryAutomation.waitingThread) return;
+
+    const filtered = msg.channel.guild.channels.filter(c => {
+      return (c instanceof Eris.CategoryChannel) && (c.name === catStr);
+    });
+
+    if (filtered.length > 0) {
+      await bot.editChannel(thread.channel_id, {
+        parentID: filtered[0].id
+      });
+    } else {
+      const newCat = await msg.channel.guild.createChannel(catStr, 4);
+
+      // might have need of this later
+      //await bot.editChannelPermission(newCat.id, config.mailGuildId, null, 1024, 'role');
+      //await bot.editChannelPermission(newCat.id, config.inboxServerPermission, 1024, null, 'role');
+      await bot.editChannel(thread.channel_id, {
+        parentID: newCat.id
+      });
+    }
+
+    thread.postSystemMessage(`Thread claimed by **${msg.author.username}#${msg.author.discriminator}**`)
+  });
+};
