cfe798216b937b42bbb2acaab48a13bc0246e79d
Bill Hellier <bill.hellier@gmail.com>
2020-02-06 20:37:25 -0500

    toggleSub method calls weren't async

--- src/modules/sub.js
+++ src/modules/sub.js
@@ -1,9 +1,9 @@
 module.exports = ({ bot, knex, config, commands }) => {
   commands.addInboxThreadCommand('sub', '[opt:string]', async (msg, args, thread) => {
     if (args.opt && !isNaN(args.opt)) {
-       await thread.postSystemMessage(thread.toggleSub(msg.author.id, args.opt));
+       await thread.postSystemMessage(await thread.toggleSub(msg.author.id, args.opt));
     } else {
-      await thread.postSystemMessage(thread.toggleSub(msg.author.id, null));
+      await thread.postSystemMessage(await thread.toggleSub(msg.author.id, null));
     }
   });
 };
