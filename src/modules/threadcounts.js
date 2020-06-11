4eadb78644790967af7a4e896078f9663fbe43de
Bill Hellier <bill.hellier@gmail.com>
2020-06-11 09:50:02 -0400

    Need to import threads in threadcounts command

--- src/modules/threadcounts.js
+++ src/modules/threadcounts.js
@@ -1,3 +1,5 @@
+const threads = require("../data/threads");
+
 module.exports = ({ bot, knex, config, commands }) => {
   commands.addInboxThreadCommand('threadcounts', [], async (msg, args, thread) => {
     thread.postSystemMessage(threads.getThreadRoles());
