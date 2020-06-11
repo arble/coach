629770509089f396dc70e6d0efad87e404fe0512
Bill Hellier <bill.hellier@gmail.com>
2020-06-11 12:27:04 -0400

    Wrong path for threads import

--- src/modules/threadcounts.js
+++ src/modules/threadcounts.js
@@ -1,8 +1,8 @@
-const threads = require("../threads");
+const threads = require("../data/threads");
 
 module.exports = ({ bot, knex, config, commands }) => {
   commands.addInboxThreadCommand('threadcounts', [], async (msg, args, thread) => {
-    const roles = await threads.getThreadRoles(); 
+    const roles = await threads.getThreadRoles();
     thread.postSystemMessage(roles.toString());
   });
 };
